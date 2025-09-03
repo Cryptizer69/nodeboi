2d9b8103364999b713e7c27a1310afce660cb739
GitHub Action <action@github.com>
2025-09-03 13:29:28 +0000

    Bump version to v1.0.16

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ PINK='\033[38;5;213m'
 
 REPO_URL="https://github.com/Cryptizer69/nodeboi.git"
 INSTALL_DIR="$HOME/.nodeboi"
-SCRIPT_VERSION="v1.0.15"
+SCRIPT_VERSION="v1.0.16"
 
 # ASCII Art function
 print_nodeboi_art() {
