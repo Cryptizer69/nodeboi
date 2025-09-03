515853bd5ce88c2b5d64b83f4f1fe613673e8366
GitHub Action <action@github.com>
2025-09-03 14:13:21 +0000

    Bump version to v2.0.5

--- nodeboi.sh
+++ nodeboi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# NODEBOI v2.0.4 - Clean 4-file structure
+# NODEBOI v2.0.5 - Clean 4-file structure
 
 set -eo pipefail
 trap 'echo "Error on line $LINENO" >&2' ERR
@@ -30,7 +30,7 @@ print_header() {
 HEADER
     echo -e "${NC}"
     echo -e "                    ${CYAN}ETHEREUM NODE AUTOMATION${NC}"
-    echo -e "                           ${YELLOW}v2.0.4${NC}"
+    echo -e "                           ${YELLOW}v2.0.5${NC}"
     echo
 }
 
@@ -120,8 +120,8 @@ case "$1" in
         echo "Testing functions:"
         echo -n "  Docker image for teku: "
         get_docker_image "teku"
-        echo -n "  Normalize version v2.0.4 for teku: "
-        normalize_version "teku" "v2.0.4"
+        echo -n "  Normalize version v2.0.5 for teku: "
+        normalize_version "teku" "v2.0.5"
         echo ""
         echo "All systems operational!"
         ;;
