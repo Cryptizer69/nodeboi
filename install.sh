ab3bc45ecc2c5d491961a583699ee418f25d6b1b
GitHub Action <action@github.com>
2025-09-03 14:31:29 +0000

    Bump version to v2.0.9

--- install.sh
+++ install.sh
@@ -148,7 +148,7 @@ prompt_version() {
             ;;
         2)
             while true; do
-                read -r -p "Enter version (e.g., v2.0.8 or 25.7.0): " selected_version
+                read -r -p "Enter version (e.g., v2.0.9 or 25.7.0): " selected_version
 
                 if [[ -z "$selected_version" ]]; then
                     echo "Version cannot be empty! Type 'cancel' to skip." >&2
