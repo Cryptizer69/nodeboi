fea11b49073258014e2cd9c154029c9c10f6e788
GitHub Action <action@github.com>
2025-09-04 12:37:12 +0000

    Bump version to v2.0.32

--- nodeboi.sh
+++ nodeboi.sh
@@ -128,8 +128,8 @@ case "$1" in
         echo "Testing functions:"
         echo -n "  Docker image for teku: "
         get_docker_image "teku"
-        echo -n "  Normalize version v2.0.31 for teku: "
-        normalize_version "teku" "v2.0.31"
+        echo -n "  Normalize version v2.0.32 for teku: "
+        normalize_version "teku" "v2.0.32"
         echo ""
         echo "All systems operational!"
         ;;
