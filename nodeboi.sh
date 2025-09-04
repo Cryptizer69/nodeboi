e3ab6cb3ae9afa280abc3a38b917d85da92930c3
Cryptizer69 <95619501+Cryptizer69@users.noreply.github.com>
2025-09-04 15:29:16 +0200

    Update SCRIPT_VERSION format to include 'v'

--- nodeboi.sh
+++ nodeboi.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 trap 'echo "Error on line $LINENO" >&2' ERR
 
-SCRIPT_VERSION="0.2.33"
+SCRIPT_VERSION="v0.2.33"
 NODEBOI_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 NODEBOI_LIB="${NODEBOI_HOME}/lib"
 
